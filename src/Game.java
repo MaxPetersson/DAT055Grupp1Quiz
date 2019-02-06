2f75ecbfc832a9029d1c2586d33c1afc2d44ce84
MaxPetersson <maxpetersson@gmail.com>
2019-02-06 16:15:09 +0100

    Moved currentQuestion logic to game

--- src/Game.java
+++ src/Game.java
@@ -4,20 +4,23 @@ import java.util.ArrayList;
 public class Game {
     ArrayList<Question> localQuestionBank = new ArrayList<Question>();
     ArrayList<Question> currentQuiz;
+    int currentQuestion;
 
 
 
+    //hardcoding of 3 questions
     private String[] qtextarray = {"En b�t k�r med en hastighet av fyra knop mot en brygga. En meter fr�n bryggan saktar b�ten ner till tre knop. Har l�ngt tid tar det att laga bryggan?",
             "What is the air-speed velocity of an unladen swallow?",
             "How much wood could a woodchuck chuck if a woodchuck could chuck wood? "};
     private String[] catArr = {"G�tor","Monty Python","Trivia"};
     private ArrayList<String> answers1;
-
     private ArrayList<String> answers2;
     private ArrayList<String> answers3;
 
     public Game (){
+        currentQuestion = 0;
 
+        //hardcoding of 3 questions
         answers1 = new ArrayList<String>();
         answers1.add("1");
 
@@ -33,14 +36,23 @@ public class Game {
         localQuestionBank.add(new Question(catArr[2], qtextarray[2], answers1));
 
 
-
-
-
+    }
+    //GETTER
+    public int getTotalNumberOfQuestionInQuiz(){
+        return currentQuiz.size();
     }
 
+    public int getCurrentQuestion(){
+        return currentQuestion;
+    }
+
+
+    //SETTER
     public void addQuestionToLocalQuestionBank (Question qToAdd){
         localQuestionBank.add(qToAdd);
     }
+
+
 
     //returns true if quiz generated successfully
     //returns false if it didnt (most likely because there wasnt enough questions
@@ -55,14 +67,16 @@ public class Game {
             for (int i = 0; i <= nrOfQuestions; i++){
                 currentQuiz.add(localQuestionBank.get(i));
             }
-
+            currentQuestion = 0;
             return true;
         }
 
     }
 
-    public Question getNextQuestion(int index){
-        return currentQuiz.get(index);
+
+
+    public Question getNextQuestion(){
+        return currentQuiz.get(currentQuestion++);
 
     }
 
