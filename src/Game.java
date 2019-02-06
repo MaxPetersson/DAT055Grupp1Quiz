67dbb274ab2a2754c506c2a8f5bc14c024a652d8
MaxPetersson <maxpetersson@gmail.com>
2019-02-06 15:10:47 +0100

    three helper methods for game class

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,38 @@
+import java.util.ArrayList;
 
 public class Game {
+    ArrayList<Question> localQuestionBank = new ArrayList<Question>();
+    ArrayList<Question> currentQuiz;
+    public Game (){
+
+    }
+
+    public void addQuestionToLocalQuestionBank (Question qToAdd){
+        localQuestionBank.add(qToAdd);
+    }
+
+    //returns true if quiz generated successfully
+    //returns false if it didnt (most likely because there wasnt enough questions
+    //Set nrOfQuestions to max?
+    public boolean generateQuiz(int nrOfQuestions){
+        if (nrOfQuestions > localQuestionBank.size()){
+            return false;
+        }
+        else
+        {
+            currentQuiz = new ArrayList<Question>();
+            for (int i = 0; i <= nrOfQuestions; i++){
+                currentQuiz.add(localQuestionBank.get(i));
+            }
+
+            return true;
+        }
+
+    }
+
+    public Question getNextQuestion(int index){
+        return currentQuiz.get(index);
+
+    }
 
 }
