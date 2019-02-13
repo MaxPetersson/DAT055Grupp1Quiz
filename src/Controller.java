ca5a766a7faaa8e695373720f3793cf3873c464e
tniclas <ntauson@gmail.com>
2019-02-13 14:55:11 +0100

    finslip_MVC
    Flyttat över flera av funktionerna i newqframe till controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,138 +1,131 @@
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 public class Controller {
 
 	/*
-	 * Instances of classes in the category View will be labelled by v_ followed by class name.
-	 * Instances of classes in the category Model will be labelled by m_ followed by class name.
+	 * Instances of classes in the category View will be labelled by v_ followed by
+	 * class name. Instances of classes in the category Model will be labelled by m_
+	 * followed by class name.
 	 */
 	private main_view v_main_view;
 	private NewQFrame v_NewQFrame;
 	private Game m_game;
-	
-	
-	//Controller constructor.
-	
-	public Controller (main_view v_main_view, NewQFrame v_NewQFrame, Game m_game) {
+
+	// Controller constructor.
+
+	public Controller(main_view v_main_view, NewQFrame v_NewQFrame, Game m_game) {
 		this.v_main_view = v_main_view;
 		this.v_NewQFrame = v_NewQFrame;
 		this.m_game = m_game;
-		
-		this.v_main_view.addNewGameListener(new NewGameListener ());
+
+		this.v_main_view.addNewGameListener(new NewGameListener());
 		this.v_main_view.addSubmitAnswerListener(new SubmitAnswerListener());
 		this.v_main_view.addNewQuestionListener(new CreateNewQuestionListener());
 		this.v_NewQFrame.addSubmitNewQuestionListener(new SubmitNewQuestionListener());
 	}
-	
 
-	///// INNER CLASS NewGameListener /////////////////////////////////////////////////////////////////////////////////
+	///// INNER CLASS NewGameListener
+	///// /////////////////////////////////////////////////////////////////////////////////
 	/*
-	 * When NEW GAME is requested.
-	 * 1. Get size of quiz from UI.
-	 * 2. Call Game to create new Quiz with this size.
-	 * 3. Get first question from Game.
-	 * 4. Tell UI to display question.
+	 * When NEW GAME is requested. 1. Get size of quiz from UI. 2. Call Game to
+	 * create new Quiz with this size. 3. Get first question from Game. 4. Tell UI
+	 * to display question.
 	 */
-	
-	//public void addNewGameListener(ActionListener e) {
-	//	newGame.addActionListener(e)
-	//}
-	
+
+	// public void addNewGameListener(ActionListener e) {
+	// newGame.addActionListener(e)
+	// }
+
 	class NewGameListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			String userInput = "";
 			int quizSize = 0;
 			try {
-				/* 1.
-				 * Get size of Quiz from UI and save in userInput.
-				 * parse to integer and save in quizSize.
+				/*
+				 * 1. Get size of Quiz from UI and save in userInput. parse to integer and save
+				 * in quizSize.
 				 */
 				userInput = v_main_view.getQuizSize();
 				quizSize = Integer.parseInt(userInput);
-				
-				//2. Call Game to create new Quiz with size quizSize.
+
+				// 2. Call Game to create new Quiz with size quizSize.
 				m_game.generateQuiz(quizSize);
-				
-				//3. Get first question from Game.
+
+				// 3. Get first question from Game.
 				Question firstQuestion = m_game.getNextQuestion();
-				
-				//4. Tell UI to display first question.
+
+				// 4. Tell UI to display first question.
 				v_main_view.changeToQuizScreen();
-				v_main_view.displayQuestion(firstQuestion.getCategory(), firstQuestion.getQuestionText(), m_game.getTotalNumberOfQuestionInQuiz(), m_game.getCurrentQuestion());
-			}
-			catch (NumberFormatException nfex) {
+				v_main_view.displayQuestion(firstQuestion.getCategory(), firstQuestion.getQuestionText(),
+						m_game.getTotalNumberOfQuestionInQuiz(), m_game.getCurrentQuestion());
+			} catch (NumberFormatException nfex) {
 				v_main_view.displayErrorMessage("Bad input: '" + userInput + "'");
-			}
-			catch (BadUserInputException buiex) {
+			} catch (BadUserInputException buiex) {
 				v_main_view.displayErrorMessage(buiex.getMessage());
 			}
 		}
-		
-		
+
 	}
-	
-	
-	
-	///// INNER CLASS SubmitAnswerListener ////////////////////////////////////////////////////////////////////////////
+
+	///// INNER CLASS SubmitAnswerListener
+	///// ////////////////////////////////////////////////////////////////////////////
 	/*
-	 * When AWNSER is submitted.
-	 * 1. Get answer from UI.
-	 * 2. Call Game to compare [submitted answer] with [question answer].
-	 * 3. Get result from Game.
-	 * 4. Tell UI to display the result.
-	 * 5. Get next question from Game.
-	 * 6. Tell UI to display question from Game.
-	 * 7. End game if there is no more questions.
+	 * When AWNSER is submitted. 1. Get answer from UI. 2. Call Game to compare
+	 * [submitted answer] with [question answer]. 3. Get result from Game. 4. Tell
+	 * UI to display the result. 5. Get next question from Game. 6. Tell UI to
+	 * display question from Game. 7. End game if there is no more questions.
 	 */
 	class SubmitAnswerListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			String userAnswer = "";
-			
+
 			try {
-				//1. get answer from user and store in userAnswer
+				// 1. get answer from user and store in userAnswer
 				userAnswer = v_main_view.getAnswer();
-				
-				//2. Compare userAwnser to the current question's answer.
+
+				// 2. Compare userAwnser to the current question's answer.
 				m_game.compareAnswer(userAnswer);
-				
-				//3 & 4. Tell UI to display the result from Game.
+
+				// 3 & 4. Tell UI to display the result from Game.
 				v_main_view.displayResult(m_game.getResult());
-				
-				
-				if(m_game.existNextQuestion()) {
-					//5. Get next question from Game.
+
+				if (m_game.existNextQuestion()) {
+					// 5. Get next question from Game.
 					Question nextQuestion = m_game.getNextQuestion();
-					//6. Tell UI to display next question. 
-					v_main_view.displayQuestion(nextQuestion.getCategory(), nextQuestion.getQuestionText(), m_game.getTotalNumberOfQuestionInQuiz(), m_game.getCurrentQuestion());
-					}
-				else {
-					//7. Show the start screen.
+					// 6. Tell UI to display next question.
+					v_main_view.displayQuestion(nextQuestion.getCategory(), nextQuestion.getQuestionText(),
+							m_game.getTotalNumberOfQuestionInQuiz(), m_game.getCurrentQuestion());
+				} else {
+					// 7. Show the start screen.
 					v_main_view.changeToStartScreen();
 				}
-					
-			}
-			catch (NullPointerException npex) {
+
+			} catch (NullPointerException npex) {
 				v_main_view.displayErrorMessage("The error message");
 			}
 		}
 	}
-	
-	///// INNER CLASS CreateNewQuestionListener ///////////////////////////////////////////////////////////////////////
+
+	///// INNER CLASS CreateNewQuestionListener
+	///// ///////////////////////////////////////////////////////////////////////
 	/*
 	 * 1. Tell UI to show NewQuestionFrame
 	 */
-	class CreateNewQuestionListener implements ActionListener{
+	class CreateNewQuestionListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			v_NewQFrame.setVisible(true);
 		}
 	}
-	
-	///// INNER CLASS SubmitNewQuestionListener ///////////////////////////////////////////////////////////////////////
+
+	///// INNER CLASS SubmitNewQuestionListener
+	///// ///////////////////////////////////////////////////////////////////////
 	/*
-	 * 1. Get Category, Question & Answer from UI.
-	 * 2. Create New Question.
-	 * 3. Tell UI to clear question form.
+	 * 1. Get Category, Question & Answer from UI. 2. Create New Question. 3. Tell
+	 * UI to clear question form.
 	 */
 	class SubmitNewQuestionListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
@@ -140,16 +133,46 @@ public class Controller {
 			String category = "";
 			ArrayList<String> answers = new ArrayList<String>();
 			Question theQuestion;
-			try {
-				question = v_NewQFrame.getQuestion();
-				category = v_NewQFrame.getCategory();
-				answers.addAll(v_NewQFrame.getAnswers());
-				theQuestion = new Question(category, question ,answers);
-				m_game.addQuestionToLocalQuestionBank(theQuestion);
+			if (fieldsOK()) {
+				try {
+					question = v_NewQFrame.getQuestion();
+					category = v_NewQFrame.getCategory();
+					answers.addAll(v_NewQFrame.getAnswers());
+					theQuestion = new Question(category, question, answers);
+					m_game.addQuestionToLocalQuestionBank(theQuestion);
+					JOptionPane.showMessageDialog(v_NewQFrame, "New question has been added!");
+				} catch (NullPointerException ex) {
+					v_NewQFrame.displayErrorMessage("The error message");
+				}
+
+				v_NewQFrame.categoryTextArea.setText(""); // clear fields. User may enter new question(?)
+				v_NewQFrame.questionTextArea.setText("");
+				v_NewQFrame.answerTextArea.setText("");
+
 			}
-			catch(NullPointerException ex) {
-				v_NewQFrame.displayErrorMessage("The error message");
-			}
+
 		}
+	}
+
+	private boolean fieldsOK() { // check if ok to submit
+		String category = v_NewQFrame.getCategory();
+		String question = v_NewQFrame.getQuestion();
+		ArrayList<String> answers = new ArrayList<String>();
+		String[] tempAnswers = v_NewQFrame.answerTextArea.getText().split(",");
+		for (String s : tempAnswers) {
+			answers.add(s);
+		}
+
+		if (category.replaceAll("\\s+", "").equals("")) {
+			v_NewQFrame.displayErrorMessage("Please enter category");
+			return false;
+		} else if (question.replaceAll("\\s+", "").equals("")) {
+			v_NewQFrame.displayErrorMessage("Please enter question");
+			return false;
+		} else if (tempAnswers[0].replaceAll("\\s+", "").equals("")) {
+			v_NewQFrame.displayErrorMessage("Please enter answer");
+			return false;
+		} else
+			return true;
 	}
 }
--- src/NewQFrame.java
+++ src/NewQFrame.java
@@ -1,18 +1,28 @@
-import javax.swing.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.*;
 import java.util.ArrayList;
 
-public class NewQFrame extends JFrame  {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
+
+public class NewQFrame extends JFrame {
 	private JPanel panel;
 	private JButton submit;
 	private JLabel categorylabel;
 	private JLabel questionlabel;
 	private JLabel answerlabel;
-	private JTextArea categoryTextArea;
-	private JTextArea questionTextArea;
-	private JTextArea answerTextArea;
+	JTextArea categoryTextArea;
+	JTextArea questionTextArea;
+	JTextArea answerTextArea;
 	private JScrollPane categoryScrollPane = new JScrollPane();
 	private JScrollPane questionScrollPane = new JScrollPane();
 	private JScrollPane answerScrollPane = new JScrollPane();
@@ -20,10 +30,9 @@ public class NewQFrame extends JFrame  {
 	private String category;
 	private String question;
 	private ArrayList<String> answers;
-	//private int numCreated = 0;	perhaps for later use
+	// private int numCreated = 0; perhaps for later use
 
-
-	//Constructor
+	// Constructor
 	public NewQFrame(String title) {
 		super(title);
 		setupPanel();
@@ -32,34 +41,27 @@ public class NewQFrame extends JFrame  {
 		this.add(panel);
 		this.pack();
 	}
-	
+
 	private void setupPanel() {
 		categorylabel = new JLabel("Category:");
 		questionlabel = new JLabel("Question:");
 		answerlabel = new JLabel("Answer:");
-		
+
 		categoryTextArea = new JTextArea(1, 25);
 		initializeTextArea(categoryTextArea, "Enter category", 1);
 		setScrollable(categoryScrollPane, categoryTextArea);
-		
+
 		questionTextArea = new JTextArea(6, 25);
 		initializeTextArea(questionTextArea, "Enter question", 6);
 		setScrollable(questionScrollPane, questionTextArea);
-		
+
 		answerTextArea = new JTextArea(6, 25);
 		initializeTextArea(answerTextArea, "Enter answer", 6);
 		setScrollable(answerScrollPane, answerTextArea);
 
 		submit = new JButton("Submit");
-		submit.setBounds(1,1,10,10);
-//		submit.addActionListener(new ActionListener() {	//When submit button is pressed, add new question with user inserted text
-//				public void actionPerformed(ActionEvent e) {
-//					if(fieldsOK()) {
-//						submitQuestion(category, question, answers);
-//					}	
-//				}
-//		});
-		
+		submit.setBounds(1, 1, 10, 10);
+
 		panel = new JPanel(new SpringLayout());
 		panel.add(categorylabel);
 		panel.add(categoryScrollPane);
@@ -67,100 +69,61 @@ public class NewQFrame extends JFrame  {
 		panel.add(questionScrollPane);
 		panel.add(answerlabel);
 		panel.add(answerScrollPane);
-		panel.add(new JLabel("")); //empty label
+		panel.add(new JLabel("")); // empty label
 		panel.add(submit);
-		SpringUtilities.makeCompactGrid(panel, 4, 2, 10, 10, 10, 10); //4 rows, 2 columns
+		SpringUtilities.makeCompactGrid(panel, 4, 2, 10, 10, 10, 10); // 4 rows, 2 columns
 	}
-	
-	
-	public String getCategory(){
+
+	public String getCategory() {
 		return categoryTextArea.getText();
 	}
-	
+
 	public String getQuestion() {
 		return questionTextArea.getText();
 	}
-	
+
 	public ArrayList<String> getAnswers() {
 		ArrayList<String> answers = new ArrayList<String>();
 		answers.add(questionTextArea.getText());
 		return answers;
 	}
-	
-	//initialize text area and add text prompt
+
+	// initialize text area and add text prompt
 	private void initializeTextArea(JTextArea textArea, String text, int rows) {
-		tp = new TextPrompt(text, textArea); 
+		tp = new TextPrompt(text, textArea);
 		tp.setForeground(Color.GRAY);
 		tp.changeStyle(Font.ITALIC);
 		textArea.setLineWrap(true);
 		textArea.setWrapStyleWord(true);
-		
-		//move focus forward to next textArea (or to the 'submit' button) when TAB key is pressed. If modifier key is held down, move focus backward.
+
+		// move focus forward to next textArea (or to the 'submit' button) when TAB key
+		// is pressed. If modifier key is held down, move focus backward.
 		textArea.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_TAB) {
-                    if (e.getModifiersEx() > 0) {
-                        textArea.transferFocusBackward();
-                    } else {
-                        textArea.transferFocus();
-                    }
-                    e.consume();
-                }
-            }
-        });
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_TAB) {
+					if (e.getModifiersEx() > 0) {
+						textArea.transferFocusBackward();
+					} else {
+						textArea.transferFocus();
+					}
+					e.consume();
+				}
+			}
+		});
 	}
-	
-	//make text area scrollable
+
+	// make text area scrollable
 	private void setScrollable(JScrollPane sp, JTextArea ta) {
 		sp.setViewportView(ta);
 		sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 	}
-	
-	//check if ok to submit
-	private boolean fieldsOK() {
-		category = categoryTextArea.getText();
-		question = questionTextArea.getText();
-		answers = new ArrayList<String>();
-		String[] tempAnswers = answerTextArea.getText().split(",");
-		for(String s: tempAnswers)
-		{
-			answers.add(s);
-		}
-		
-		if (category.replaceAll("\\s+","").equals("")) {
-			displayErrorMessage("Please enter category");
-			return false;
-		}
-		else if (question.replaceAll("\\s+","").equals("")) {
-			displayErrorMessage("Please enter question");
-			return false;
-		}
-		else if (tempAnswers[0].replaceAll("\\s+","").equals("")) {
-			displayErrorMessage("Please enter answer");
-			return false;
-		}
-		else return true;
-	}
-	
+
 	public void addSubmitNewQuestionListener(ActionListener listenForSubmitQuestion) {
 		submit.addActionListener(listenForSubmitQuestion);
 	}
-	
+
 	public void displayErrorMessage(String error) {
 		JOptionPane.showMessageDialog(this, error);
 	}
-	
-	private void submitQuestion(String category, String question, ArrayList<String> answers) {
-		
-		Question newQ = new Question(category, question, answers);
-		//tempArrayListofQuestions.add(newQ);		store newQ somewhere
-		JOptionPane.showMessageDialog(this, "New question created in category: "+category);
-		categoryTextArea.setText(""); //clear fields. User may enter new question(?)
-		questionTextArea.setText("");
-		answerTextArea.setText("");
-		//numCreated++;
-	}
-
 }
-
