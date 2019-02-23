322eb36a2ea397ef2274e8da3442604066c2cc90
Thureborn <toscar@student.chalmers.se>
2019-02-23 01:49:00 +0100

    Controller almost done

--- src/Controller.java
+++ src/Controller.java
@@ -7,43 +7,39 @@ import javax.swing.JOptionPane;
 public class Controller {
 
 	/*
-	 * Instances of classes in the category View will be labelled by v_ followed by
-	 * class name. Instances of classes in the category Model will be labelled by m_
-	 * followed by class name.
+	 * 
 	 */
-	private main_view v_main_view;
-	private NewQFrame v_NewQFrame;
-	private Game m_game;
+	private main_view mainWindow;
+	private NewQFrame newQuestionFrame;
+	private Game game;
 
 	// Controller constructor.
 
-	public Controller(main_view v_main_view, NewQFrame v_NewQFrame, Game m_game) {
-		this.v_main_view = v_main_view;
-		this.v_NewQFrame = v_NewQFrame;
-		this.m_game = m_game;
+	public Controller(main_view mainWindow, NewQFrame newQuestionFrame, Game game) {
+		this.mainWindow = mainWindow;
+		this.newQuestionFrame = newQuestionFrame;
+		this.game = game;
 
-		this.v_main_view.addNewGameListener(new NewGameListener());
-		this.v_main_view.addSubmitAnswerListener(new SubmitAnswerListener());
-		this.v_main_view.addNewQuestionListener(new CreateNewQuestionListener());
-		this.v_NewQFrame.addSubmitNewQuestionListener(new SubmitNewQuestionListener());
-		this.v_main_view.addEditQuestionsListener(new EditQuestionsListener());
-		this.v_main_view.addDeleteQuestionsListener(new DeleteQuestionsListener());
-		v_main_view.setCategories(m_game.fetchCategories());
-		m_game.fetchQuestions();
+		this.mainWindow.addNewGameListener(new NewGameListener());
+		this.mainWindow.addSubmitAnswerListener(new SubmitAnswerListener());
+		this.mainWindow.addNewQuestionListener(new CreateNewQuestionListener());
+		this.newQuestionFrame.addSubmitNewQuestionListener(new SubmitNewQuestionListener());
+		this.mainWindow.addEditQuestionsListener(new EditQuestionsListener());
+		this.mainWindow.addDeleteQuestionsListener(new DeleteQuestionsListener());
+		mainWindow.setCategories(game.fetchCategories());
+		game.fetchQuestions();
 
 	}
 
 	///// INNER CLASS NewGameListener
 	///// /////////////////////////////////////////////////////////////////////////////////
 	/*
-	 * When NEW GAME is requested. 1. Get size of quiz from UI. 2. Call Game to
-	 * create new Quiz with this size. 3. Get first question from Game. 4. Tell UI
-	 * to display question.
+	 * When NEW GAME is requested. 
+	 * 1. Get size of quiz from UI. 
+	 * 2. Call Game to create new Quiz with this size in selected categories. 
+	 * 3. Set first question in Game.
+	 * 4. Change to quiz screen in MainWindow. 
 	 */
-
-	// public void addNewGameListener(ActionListener e) {
-	// newGame.addActionListener(e)
-	// }
 
 	class NewGameListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
@@ -51,34 +47,31 @@ public class Controller {
 			int quizSize = 0;
 			try {
 				/*
-				 * 1. Get size of Quiz from UI and save in userInput. parse to integer and save
-				 * in quizSize.
+				 * 1. Get size of Quiz from UI and save in userInput. parse to integer and save in quizSize.
 				 */
-				userInput = v_main_view.getQuizSize();
+				userInput = mainWindow.getQuizSize();
 				quizSize = Integer.parseInt(userInput);
+				
+				//2. Call Game to create new Quiz with this quizSize in selected categories.
+				if (!mainWindow.getSelectedCategory().isEmpty()) {
+					// Generating quiz with size = quizSise and chosen categories.
+					game.generateQuiz(quizSize, mainWindow.getSelectedCategory());
 
-				// 2. Call Game to create new Quiz with size quizSize if atleast one category
-				// has been choosen
-				if (!v_main_view.getSelectedCategory().isEmpty()) {
-					m_game.generateQuiz(quizSize, v_main_view.getSelectedCategory());
+					// 3. Set first Question in Game.
+					game.setNextQuestion();
 
-					// 3. Get first question from Game.
-					// Question firstQuestion = m_game.getNextQuestion();
-					m_game.setNextQuestion();
-
-					// 4. Tell UI to display first question.
-					v_main_view.changeToQuizScreen();
-//						v_main_view.displayQuestion(firstQuestion.getCategory(), firstQuestion.getQuestionText(),
-//						m_game.getTotalNumberOfQuestionInQuiz(), m_game.getCurrentQuestion());
+					// 4. Tell UI to display quiz screen.
+					mainWindow.changeToQuizScreen();;
 
 				} else {
-					v_main_view.displayErrorMessage("Choose a category");
+					// If no category is chosen, throw BadUserInputException.
+					throw new BadUserInputException("Choose at least one category");
 
 				}
 			} catch (NumberFormatException nfex) {
-				v_main_view.displayErrorMessage("Bad input: '" + userInput + "'");
+				mainWindow.displayErrorMessage("You must enter a number of questions");
 			} catch (BadUserInputException buiex) {
-				v_main_view.displayErrorMessage(buiex.getMessage());
+				mainWindow.displayErrorMessage(buiex.getMessage());
 			}
 		}
 
@@ -87,10 +80,11 @@ public class Controller {
 	///// INNER CLASS SubmitAnswerListener
 	///// ////////////////////////////////////////////////////////////////////////////
 	/*
-	 * When ANSWER is submitted. 1. Get answer from UI. 2. Call Game to compare
-	 * [submitted answer] with [question answer]. 3. Get result from Game. 4. Tell
-	 * UI to display the result. 5. Get next question from Game. 6. Tell UI to
-	 * display question from Game. 7. End game if there is no more questions.
+	 * When ANSWER is submitted.
+	 * 1. Get answer from UI. 
+	 * 2. Call Game to compare the submitted answer with question answer.
+	 * 3. Set next question in Game.  
+	 * 4. Show quiz score if there is no more questions.
 	 */
 	class SubmitAnswerListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
@@ -98,31 +92,21 @@ public class Controller {
 
 			try {
 				// 1. get answer from user and store in userAnswer
-				userAnswer = v_main_view.getAnswer();
+				userAnswer = mainWindow.getAnswer();
 
 				// 2. Compare userAwnser to the current question's answer.
-				m_game.compareAnswer(userAnswer);
-
-				// 3 & 4. Tell UI to display the result from Game.
-				// v_main_view.displayResult(m_game.getResult());
-
-				if (m_game.existNextQuestion()) {
-					// 5. Get next question from Game.
-					// Question nextQuestion = m_game.getNextQuestion();
-					// 6. Tell UI to display next question.
-					// v_main_view.displayQuestion(nextQuestion.getCategory(),
-					// nextQuestion.getQuestionText(),
-					// m_game.getTotalNumberOfQuestionInQuiz(), m_game.getCurrentQuestion());
-
-					m_game.setNextQuestion();
-
+				game.compareAnswer(userAnswer);
+				
+				if (game.existNextQuestion()) {
+					// 3. Set next question in Game.
+					game.setNextQuestion();
 				} else {
-					// 7. Show the start screen.
-					v_main_view.printResult(m_game.results, m_game.userAnswers, m_game.currentQuiz);
+					// 4. Display the quiz score in MainWindow.
+					mainWindow.printResult(game.results, game.userAnswers, game.currentQuiz);
 				}
 
 			} catch (NullPointerException npex) {
-				v_main_view.displayErrorMessage("The error message");
+				mainWindow.displayErrorMessage("The error message");
 			}
 		}
 	}
@@ -134,15 +118,16 @@ public class Controller {
 	 */
 	class CreateNewQuestionListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			v_NewQFrame.setVisible(true);
+			newQuestionFrame.setVisible(true);
 		}
 	}
 
 	///// INNER CLASS SubmitNewQuestionListener
 	///// ///////////////////////////////////////////////////////////////////////
 	/*
-	 * 1. Get Category, Question & Answer from UI. 2. Create New Question. 3. Tell
-	 * UI to clear question form.
+	 * 1. Get Category, Question & Answer from UI. 
+	 * 2. Tell game to create new Question.
+	 * 3. Tell UI to clear question form.
 	 */
 	class SubmitNewQuestionListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
@@ -153,25 +138,25 @@ public class Controller {
 
 			if (fieldsOK()) {
 				try {
-					question = v_NewQFrame.getQuestion();
-					category = v_NewQFrame.getCategory().substring(0, 1).toUpperCase() // sets first letter to uppercase
+					question = newQuestionFrame.getQuestion();
+					category = newQuestionFrame.getCategory().substring(0, 1).toUpperCase() // sets first letter to uppercase
 																						// and leaves rest alone
-							+ v_NewQFrame.getCategory().substring(1);
-					answers.addAll(v_NewQFrame.getAnswers());
+							+ newQuestionFrame.getCategory().substring(1);
+					answers.addAll(newQuestionFrame.getAnswers());
 					theQuestion = new Question(category, question, answers);
-					m_game.addQuestionToQuestionBank(theQuestion);
+					game.addQuestionToQuestionBank(theQuestion);
 
-					if (!m_game.fetchCategories().contains(theQuestion.getCategory())) {
-						m_game.addCategory(theQuestion.getCategory());
+					if (!game.fetchCategories().contains(theQuestion.getCategory())) {
+						game.addCategory(theQuestion.getCategory());
 
 					}
 
-					JOptionPane.showMessageDialog(v_NewQFrame, "New question has been added!");
+					JOptionPane.showMessageDialog(newQuestionFrame, "New question has been added!");
 				} catch (NullPointerException ex) {
-					v_NewQFrame.displayErrorMessage("The error message");
+					newQuestionFrame.displayErrorMessage("The error message");
 				}
 
-				v_NewQFrame.clearWindow();
+				newQuestionFrame.clearWindow();
 
 			}
 
@@ -181,8 +166,8 @@ public class Controller {
 	class EditQuestionsListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 
-			ArrayList<Question> questions = m_game.fetchQuestions();
-			v_main_view.printQuestionsList(questions);
+			ArrayList<Question> questions = game.fetchQuestions();
+			mainWindow.printQuestionsList(questions);
 		}
 
 	}
@@ -190,33 +175,48 @@ public class Controller {
 	class DeleteQuestionsListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 
-			String[] parts = v_main_view.getSelectedQuestion().substring(3).split(" - ");
+			String[] parts = mainWindow.getSelectedQuestion().substring(3).split(" - ");
 
-			m_game.deleteQuestion(parts[0], parts[1]);
+			game.deleteQuestion(parts[0], parts[1]);
 
 		}
 
 	}
 
 	private boolean fieldsOK() { // check if ok to submit
-		String category = v_NewQFrame.getCategory();
-		String question = v_NewQFrame.getQuestion();
+		String category = newQuestionFrame.getCategory();
+		String question = newQuestionFrame.getQuestion();
 		ArrayList<String> answers = new ArrayList<String>();
-		String[] tempAnswers = v_NewQFrame.answerTextArea.getText().split(",");
+		String[] tempAnswers = newQuestionFrame.answerTextArea.getText().split(",");
 		for (String s : tempAnswers) {
 			answers.add(s);
 		}
 
 		if (category.replaceAll("\\s+", "").equals("")) {
-			v_NewQFrame.displayErrorMessage("Please enter category");
+			newQuestionFrame.displayErrorMessage("Please enter category");
 			return false;
 		} else if (question.replaceAll("\\s+", "").equals("")) {
-			v_NewQFrame.displayErrorMessage("Please enter question");
+			newQuestionFrame.displayErrorMessage("Please enter question");
 			return false;
 		} else if (tempAnswers[0].replaceAll("\\s+", "").equals("")) {
-			v_NewQFrame.displayErrorMessage("Please enter answer");
+			newQuestionFrame.displayErrorMessage("Please enter answer");
 			return false;
 		} else
 			return true;
 	}
+
+private boolean checkQuestionFields(String category, String question, ArrayList<String> answers) 
+throws BadUserInputException {
+	
+	if (category.replaceAll("\\s+", "").equals("")) {
+		throw new BadUserInputException("Enter a category");
+	}
+	else if (question.replaceAll("\\s+", "").equals("")) {
+		throw new BadUserInputException("Enter a question");
+	} 
+	else if (answers.get(0).equals("")) {
+		throw new BadUserInputException("Enter a category");
+	}
+	return true;
+}
 }
