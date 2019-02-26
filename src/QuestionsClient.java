4f05c2f2ed66808f7df0ea1a004d79971aaec518
tniclas <ntauson@gmail.com>
2019-02-26 12:51:29 +0100

    radering av förkodade frågor

--- src/QuestionsClient.java
+++ src/QuestionsClient.java
@@ -12,37 +12,10 @@ public class QuestionsClient extends Observable {
 
 	private ArrayList<Question> questionBank = new ArrayList<Question>();
 	private ArrayList<String> categories;
-	// hardcoding of 3 questions
-	private String[] qtextarray = {
-			"En båt kör med en hastighet av fyra knop mot en brygga. En meter från bryggan saktar båten ner till tre knop. Har långt tid tar det att laga bryggan?",
-			"What is the air-speed velocity of an unladen swallow?",
-			"How much wood could a woodchuck chuck if a woodchuck could chuck wood? " };
-
-	private ArrayList<String> answers1;
-	private ArrayList<String> answers2;
-	private ArrayList<String> answers3;
 
 	public QuestionsClient() {
 
-		// hardcoding of 3 questions
-		answers1 = new ArrayList<String>();
-		answers1.add("1");
-
-		answers2 = new ArrayList<String>();
-		answers2.add("1");
-
-		answers3 = new ArrayList<String>();
-		answers3.add("1");
-
 		categories = new ArrayList<String>();
-		categories.add("Gåtor");
-		categories.add("Monty Python");
-		categories.add("Trivia");
-
-		// Remove later
-		questionBank.add(new Question(categories.get(0), qtextarray[0], answers1));
-		questionBank.add(new Question(categories.get(1), qtextarray[1], answers2));
-		questionBank.add(new Question(categories.get(2), qtextarray[2], answers3));
 
 	}
 
@@ -89,8 +62,7 @@ public class QuestionsClient extends Observable {
 		setChanged();
 
 		notifyObservers(questionBank);
-		setChanged();
-		notifyObservers(questionBank);
+
 	}
 
 	// Adds a question to the question bank.
@@ -136,7 +108,9 @@ public class QuestionsClient extends Observable {
 	// Add category to categories then notify observers with categories.
 	public void loadCategories() {
 
-		categories.clear();
+		if (!categories.isEmpty()) {
+			categories.clear();
+		}
 
 		for (Question i : questionBank) {
 
